3232731d03021d214ce1b2c35c43f85cc8f16763
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2022-10-09 12:11:27 -0700

    add /demo

--- src/inOneWeekend/main.cpp
+++ src/inOneWeekend/main.cpp
@@ -35,8 +35,11 @@ int main() {
 
     // World
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.25));
+    world.add(make_shared<sphere>(point3(0.6,0,-1), 0.25));
+    world.add(make_shared<sphere>(point3(0,-100.25,-1), 100));
 
     // Camera
 
--- src/inOneWeekend/material.h
+++ src/inOneWeekend/material.h
@@ -12,4 +12,5 @@ class material {
         ) const = 0;
 };
 
+
 #endif
