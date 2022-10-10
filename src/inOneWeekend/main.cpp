8c0fb146c893fba4b726157dcaeb29f7d82bef9b
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2022-10-09 19:48:39 -0700

    defocus added

--- src/inOneWeekend/main.cpp
+++ src/inOneWeekend/main.cpp
@@ -67,8 +67,13 @@ int main() {
     // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
+    point3 lookfrom(-2,2,1);
+    point3 lookat(0,0,-1);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = (lookfrom-lookat).length();
+    auto aperture = 1.0;
 
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
+    camera cam(point3(-2,2,1), lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Render
 
