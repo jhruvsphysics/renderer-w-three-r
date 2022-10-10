cfb020690bc35fa81e5c26c0f732d0f9c5ff79bf
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2022-10-09 17:17:02 -0700

    total internal reflection, schlick approx

--- src/inOneWeekend/main.cpp
+++ src/inOneWeekend/main.cpp
@@ -44,27 +44,28 @@ int main() {
     // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    // demo
-    auto material_center = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_ground  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.3);
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 1.0);
+    // // demo
+    // auto material_center = make_shared<dielectric>(1.5);
+    // auto material_right = make_shared<dielectric>(1.5);
+    // // auto material_right = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    // auto material_ground  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.3);
+    // auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 1.0);
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.25, material_center));
-    world.add(make_shared<sphere>(point3(0.6,0,-1), 0.25, material_right));
-    world.add(make_shared<sphere>(point3(-0.6,0,-1), 0.25, material_left));
-    world.add(make_shared<sphere>(point3(0,-100.25,-1), 100, material_ground));
+    // world.add(make_shared<sphere>(point3(0,0,-1), 0.25, material_center));
+    // world.add(make_shared<sphere>(point3(0.6,0,-1), 0.25, material_right));
+    // world.add(make_shared<sphere>(point3(-0.6,0,-1), 0.25, material_left));
+    // world.add(make_shared<sphere>(point3(0,-100.25,-1), 100, material_ground));
 
     // Testing scenes:
-    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    // auto material_center = make_shared<dielectric>(1.5);
-    // auto material_left   = make_shared<dielectric>(1.5);
-    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<dielectric>(1.5);
+    auto material_left   = make_shared<dielectric>(1.5);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
 
