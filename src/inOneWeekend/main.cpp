4d625a8b89a24fcce1e66b7a27e71d9b3600944c
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2022-10-10 10:07:22 -0700

    defocus blurs

--- src/inOneWeekend/main.cpp
+++ src/inOneWeekend/main.cpp
@@ -71,7 +71,7 @@ int main() {
     point3 lookat(0,0,-1);
     vec3 vup(0,1,0);
     auto dist_to_focus = (lookfrom-lookat).length();
-    auto aperture = 1.0;
+    auto aperture = 2.0;
 
     camera cam(point3(-2,2,1), lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
